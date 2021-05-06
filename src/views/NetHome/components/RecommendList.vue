6d8e70758e05919eff54d2d5c135db647bf196a9
lizhongze <352840359@qq.com>
2021-05-06 20:22:19 +0800

    111

---
+++ src/views/NetHome/components/RecommendList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <base-section
+      :recommend-list="recommendList"
+      title="推荐歌单"
+    ></base-section>
+  </div>
+</template>
+
+<script>
+import BaseSection from "@/components/BaseSection";
+export default {
+  props: {
+    recommendList: {
+      type: Array,
+    },
+  },
+  name: "Recommendlist",
+  components: {
+    BaseSection,
+  },
+  data() {
+    return {
+      arr: [],
+    };
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style lang="less" scoped>
+</style>
